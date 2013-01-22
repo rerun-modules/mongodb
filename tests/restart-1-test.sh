70725baa5a434c8e5467b80e4c72e3a6630de507
Lee Thompson <stagr.lee@gmail.com>
2013-01-22 16:52:29 -0600

    add restart template

---
+++ tests/restart-1-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the `restart` command.
+#    
+#/ usage:  rerun stubbs:test -m mongodb -p restart [--answers <>]
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "restart"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
